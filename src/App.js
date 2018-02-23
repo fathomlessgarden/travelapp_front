c83e50ed73082f9156d5a3d4b3ec6e613870787c
derekbmcintire <derekbmcintire@gmail.com>
2018-02-23 13:54:27 -0500

    Add React-Bootstrap dependency
    - npm install react-bootstrap
    - test functionality with dropdown button in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, Jumbotron, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+
+        <DropdownButton
+          bsStyle='primary'
+          title='A Button'
+        >
+          <MenuItem eventKey="1">Action</MenuItem>
+          <MenuItem eventKey="2">Another action</MenuItem>
+          <MenuItem eventKey="3" active>
+            Active Item
+          </MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey="4">Separated link</MenuItem>
+        </DropdownButton>
+
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
