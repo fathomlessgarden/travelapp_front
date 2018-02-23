2ddbd1ecd358630c20143e9bd371e627459936d9
derekbmcintire <derekbmcintire@gmail.com>
2018-02-23 14:11:08 -0500

    Add Header component
    - create new Header component in new components folder
    - add react-bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, Jumbotron, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import './App.css';
+import Header from './components/header.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-
-        <DropdownButton
-          bsStyle='primary'
-          title='A Button'
-        >
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3" active>
-            Active Item
-          </MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-        </DropdownButton>
-
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Header />
     );
   }
 }
